a7ee9dc6ba87125d09bd2156be57c44b05ea97c8
U-DESKTOP-5IRVEEM\theun <theunis1g1coetzee@gmail.com>
2019-06-30 17:37:31 +0200

    Create printf

---
+++ ft_printf.c
@@ -0,0 +1,7 @@
+#include "ft_printf.h"	//#include directive tells the preprocessor to GRAB text 
+			//of a file and place it directly into the current file!!
+
+int	ft_printf(const char *format, ...)
+{
+
+}
